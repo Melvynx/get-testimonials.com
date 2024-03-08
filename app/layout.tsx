10e8d59d6e48d5da50d52788d05280e9d2e6a914
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-03-08 15:52:45 +0800

    fix warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { getServerUrl } from "@/get-server-url";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -9,6 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "get-testimonials.com",
   description: "Get more rewiews and money !",
+  metadataBase: new URL(getServerUrl()),
 };
 
 export default function RootLayout({
